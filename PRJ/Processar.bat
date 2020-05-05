d9d3cb4481fd072ffb8511a2f4c250c711c9a1c1
Bruno Laino de Andrade <brunolaino_andrade99@hotmail.com>
2020-05-05 18:52:58 -0300

    Script in Powershell mp4

--- PRJ/Processar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-ren Entrada\Audio\*.mp3 Audio.mp3
-ren Entrada\Imagem\*.jpg Imagem.jpg
-ffmpeg -loop 1 -i Entrada\Imagem\Imagem.jpg -i Entrada\Audio\Audio.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest -s hd1080 Saida\Video.mp4 
-move Entrada\Audio\Audio.mp3 Processados\Audio\Audio-%date:~0,2%-%date:~3,2%-%date:~6,10%-%time:~0,2%-%time:~3,2%.mp3
-move Entrada\Imagem\Imagem.jpg Processados\Imagem\Imagem-%date:~0,2%-%date:~3,2%-%date:~6,10%-%time:~0,2%-%time:~3,2%.jpg
-copy Saida\Video.mp4 Processados\Video\Video-%date:~0,2%-%date:~3,2%-%date:~6,10%-%time:~0,2%-%time:~3,2%.mp4
-
---
+++ PRJ/ProcessarMP4.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell ./Conversor/ConversorMP4.ps1
